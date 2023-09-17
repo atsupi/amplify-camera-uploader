da9abc608c50b6d3e1c9e29e7d10b294bf02a32c
atsupi <atsupi@gmail.com>
2023-09-18 02:16:10 +0900

    Added a button to represent alterate camera input.

--- src/pages/UploaderPage.tsx
+++ src/pages/UploaderPage.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Storage } from 'aws-amplify';
 import "./UploaderPage.css";
 
 export default function UploaderPage() {
     const [percentage, setPercentage] = useState("0%");
+    const [cameraImage, setCameraImage] = useState("No image");
     const isMobile = window.navigator.userAgent.toLowerCase().includes("mobile");
+    const uploadCamera = useRef<HTMLInputElement | null>(null);
+    
     const ChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
       console.log(event);
       console.log(event.target.value);
@@ -15,6 +18,7 @@ export default function UploaderPage() {
           filename = "image" + id + ".jpg";
         }
         console.log("filename", filename);
+        setCameraImage(filename);
         Storage.put("input/"+filename, event.target.files[0], {
           level: "public",
           contentType: event.target.files[0].type,
@@ -30,11 +34,18 @@ export default function UploaderPage() {
       }
     };
 
+    function onCaptureCamera(event: any) {
+      console.log(event);
+      if (uploadCamera.current) {
+        uploadCamera.current.click();
+      }
+    }
     return (
     <>
             <div className={isMobile? "uploadCameraDiv enabled": "uploadCameraDiv disabled"}>
-              <input id="uploadCamera" type="file" name="image" accept="image/*" capture onChange={ChangeInput} disabled={!isMobile}></input>
-              <p>{percentage}</p>
+              <input id="uploadCamera" type="file" name="image" accept="image/*" capture onChange={ChangeInput} disabled={!isMobile} ref={uploadCamera}></input>
+              <button onClick={onCaptureCamera} disabled={!isMobile}>Take a photo</button>
+              <p>{cameraImage}</p>
             </div>
             <div className="uploadFileDiv">
               <input id="uploadFile" type="file" name="image" accept="image/*" onChange={ChangeInput}></input>
